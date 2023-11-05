d3b14b27a4a49f5988745759254563d80952f816
Вадим <107673843+VadimAbergard@users.noreply.github.com>
2023-11-05 11:26:02 +0300

    Update Program.cs
    add cooldown

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,19 +9,23 @@ namespace Bubla
         private static void Main(string[] args)
         {
 
-            const int seconds = 30;
+            int seconds = 30;
 
             long time = 0;
 
-            string[] timeText = ReadFile();
-
-            if(timeText != null) time = int.Parse(timeText[2]) + int.Parse(timeText[1]) * 60 + int.Parse(timeText[0]) * 3600;
+            string text = ReadFile();
+            if (text != null)
+            {
+                string[] timeText = text.Split('|')[1].Split('.');
+                seconds = int.Parse(text.Split('|')[0]);
+                time = int.Parse(timeText[2]) + int.Parse(timeText[1]) * 60 + int.Parse(timeText[0]) * 3600;
+            }
 
             while (true)
             {
                 Thread.Sleep(seconds * 1000);
                 time += seconds;
-                WriteFile(time);
+                WriteFile(time, seconds);
                 Console.WriteLine(time);
             }
         }
@@ -50,21 +54,21 @@ namespace Bubla
             return $"{hour}.{minute}.{second}";
         }
 
-        private static void WriteFile(long time)
+        private static void WriteFile(long time,int seconds)
         {
             if (File.Exists("timeInGame.txt")) File.Delete("timeInGame.txt");
 
             FileStream fileScript = new FileStream("timeInGame.txt", FileMode.CreateNew);
-            string textTime = FinalTime(time);
+            string textTime = $"{seconds}|{FinalTime(time)}";
             fileScript.Write(Encoding.UTF8.GetBytes(textTime), 0, textTime.Length);
             fileScript.Close();
         }
 
-        private static string[] ReadFile()
+        private static string ReadFile()
         {
             if (!File.Exists("timeInGame.txt")) return null;
             string text = File.ReadAllText("timeInGame.txt");
-            return text.Split('.');
+            return text;
         }
     }
 }
